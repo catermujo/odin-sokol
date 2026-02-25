bd3aaa63ab39cc64ed0649bdbd79c12df0c6f879
Isabella Basso <isabellabdoamaral@gmail.com>
2026-02-25 18:12:22 -0300

    web: WGPU

--- build_wasm.bat
+++ build_wasm.bat
@@ -2,18 +2,28 @@
 
 set sources=log app gfx glue time audio debugtext shape gl
 
-REM Debug
+call :build_backend gl SOKOL_GLES3
+call :build_backend wgpu SOKOL_WGPU --use-port=emdawnwebgpu
+goto :eof
+
+:build_backend
+set backend=%1
+set define=%2
+shift
+shift
+set extra_flags=%*
+
 for %%s in (%sources%) do (
-    echo %%s\sokol_%%s_wasm_gl_debug.a
-    call emcc -c -g -DIMPL -DSOKOL_GLES3 c\sokol_%%s.c
-    call emar rcs %%s\sokol_%%s_wasm_gl_debug.a sokol_%%s.o
+    echo %%s\sokol_%%s_wasm_%backend%_debug.a
+    call emcc -c -g -DIMPL -D%define% %extra_flags% c\sokol_%%s.c
+    call emar rcs %%s\sokol_%%s_wasm_%backend%_debug.a sokol_%%s.o
     del sokol_%%s.o
 )
 
-REM Release
 for %%s in (%sources%) do (
-    echo %%s\sokol_%%s_wasm_gl_release.a
-    call emcc -c -O2 -DNDEBUG -DIMPL -DSOKOL_GLES3 c\sokol_%%s.c
-    call emar rcs %%s\sokol_%%s_wasm_gl_release.a sokol_%%s.o
+    echo %%s\sokol_%%s_wasm_%backend%_release.a
+    call emcc -c -O2 -DNDEBUG -DIMPL -D%define% %extra_flags% c\sokol_%%s.c
+    call emar rcs %%s\sokol_%%s_wasm_%backend%_release.a sokol_%%s.o
     del sokol_%%s.o
 )
+goto :eof
--- build_wasm.sh
+++ build_wasm.sh
@@ -3,18 +3,26 @@ set -e
 
 declare -a libs=("log" "gfx" "app" "glue" "time" "audio" "debugtext" "shape" "gl")
 
-for l in "${libs[@]}"
-do
-    echo "${l}/sokol_${l}_wasm_gl_debug.a"
-    emcc -c -g -DIMPL -DSOKOL_GLES3 c/sokol_$l.c
-    emar rcs $l/sokol_${l}_wasm_gl_debug.a sokol_$l.o
-    rm sokol_$l.o
-done
+build_backend() {
+    local backend="$1"
+    local define="$2"
+    shift 2
+    local extra_flags=("$@")
 
-for l in "${libs[@]}"
-do
-    echo "${l}/sokol_${l}_wasm_gl_release.a"
-    emcc -c -O2 -DNDEBUG -DIMPL -DSOKOL_GLES3 c/sokol_$l.c
-    emar rcs $l/sokol_${l}_wasm_gl_release.a sokol_$l.o
-    rm sokol_$l.o
-done
+    for l in "${libs[@]}"; do
+        echo "${l}/sokol_${l}_wasm_${backend}_debug.a"
+        emcc -c -g -DIMPL -D"${define}" "${extra_flags[@]}" c/sokol_"$l".c
+        emar rcs "$l"/sokol_"$l"_wasm_"$backend"_debug.a sokol_"$l".o
+        rm sokol_"$l".o
+    done
+
+    for l in "${libs[@]}"; do
+        echo "${l}/sokol_${l}_wasm_${backend}_release.a"
+        emcc -c -O2 -DNDEBUG -DIMPL -D"${define}" "${extra_flags[@]}" c/sokol_"$l".c
+        emar rcs "$l"/sokol_"$l"_wasm_"$backend"_release.a sokol_"$l".o
+        rm sokol_"$l".o
+    done
+}
+
+build_backend gl SOKOL_GLES3
+build_backend wgpu SOKOL_WGPU --use-port=emdawnwebgpu
