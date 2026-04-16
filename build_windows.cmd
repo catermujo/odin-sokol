dcfc17785d4b1b36a3f63c9d5d1aecfbd028dadc
Isabella Basso <isabellabdoamaral@gmail.com>
2026-04-16 20:21:18 -0300

    fix windows sokol build script and refresh dlls

--- build_windows.cmd
+++ build_windows.cmd
@@ -1,45 +1,70 @@
 @echo off
 
+setlocal EnableDelayedExpansion
+
+call :ensure_msvc || exit /b 1
+
 set sources=log app gfx glue time audio debugtext shape gl
 
 REM D3D11 Debug
 for %%s in (%sources%) do (
-    cl /c /D_DEBUG /DIMPL /DSOKOL_D3D11 c\sokol_%%s.c /Z7
-    lib /OUT:%%s\sokol_%%s_windows_x64_d3d11_debug.lib sokol_%%s.obj
-    del sokol_%%s.obj
+    cl /c /D_DEBUG /DIMPL /DSOKOL_D3D11 c\sokol_%%s.c /Z7 || exit /b 1
+    lib /OUT:%%s\sokol_%%s_windows_x64_d3d11_debug.lib sokol_%%s.obj || exit /b 1
+    if exist sokol_%%s.obj del sokol_%%s.obj
 )
 
 REM D3D11 Release
 for %%s in (%sources%) do (
-    cl /c /O2 /DNDEBUG /DIMPL /DSOKOL_D3D11 c\sokol_%%s.c
-    lib /OUT:%%s\sokol_%%s_windows_x64_d3d11_release.lib sokol_%%s.obj
-    del sokol_%%s.obj
+    cl /c /O2 /DNDEBUG /DIMPL /DSOKOL_D3D11 c\sokol_%%s.c || exit /b 1
+    lib /OUT:%%s\sokol_%%s_windows_x64_d3d11_release.lib sokol_%%s.obj || exit /b 1
+    if exist sokol_%%s.obj del sokol_%%s.obj
 )
 
 REM GL Debug
 for %%s in (%sources%) do (
-    cl /c /D_DEBUG /DIMPL /DSOKOL_GLCORE c\sokol_%%s.c /Z7
-    lib /OUT:%%s\sokol_%%s_windows_x64_gl_debug.lib sokol_%%s.obj
-    del sokol_%%s.obj
+    cl /c /D_DEBUG /DIMPL /DSOKOL_GLCORE c\sokol_%%s.c /Z7 || exit /b 1
+    lib /OUT:%%s\sokol_%%s_windows_x64_gl_debug.lib sokol_%%s.obj || exit /b 1
+    if exist sokol_%%s.obj del sokol_%%s.obj
 )
 
 REM GL Release
 for %%s in (%sources%) do (
-    cl /c /O2 /DNDEBUG /DIMPL /DSOKOL_GLCORE c\sokol_%%s.c
-    lib /OUT:%%s\sokol_%%s_windows_x64_gl_release.lib sokol_%%s.obj
-    del sokol_%%s.obj
+    cl /c /O2 /DNDEBUG /DIMPL /DSOKOL_GLCORE c\sokol_%%s.c || exit /b 1
+    lib /OUT:%%s\sokol_%%s_windows_x64_gl_release.lib sokol_%%s.obj || exit /b 1
+    if exist sokol_%%s.obj del sokol_%%s.obj
 )
 
 REM D3D11 Debug DLL
-cl /D_DEBUG /DIMPL /DSOKOL_DLL /DSOKOL_D3D11 c\sokol.c /Z7 /LDd /MDd /DLL /Fe:sokol_dll_windows_x64_d3d11_debug.dll /link /INCREMENTAL:NO
+cl /D_DEBUG /DIMPL /DSOKOL_DLL /DSOKOL_D3D11 c\sokol.c /Z7 /LDd /MDd /DLL /Fe:sokol_dll_windows_x64_d3d11_debug.dll /link /INCREMENTAL:NO || exit /b 1
 
 REM D3D11 Release DLL
-cl /D_DEBUG /DIMPL /DSOKOL_DLL /DSOKOL_D3D11 c\sokol.c /LD /MD /DLL /Fe:sokol_dll_windows_x64_d3d11_release.dll /link /INCREMENTAL:NO
+REM DUMBAI: Keep the release DLLs aligned with the release static libraries by compiling them without debug defines.
+cl /DNDEBUG /DIMPL /DSOKOL_DLL /DSOKOL_D3D11 c\sokol.c /LD /MD /DLL /Fe:sokol_dll_windows_x64_d3d11_release.dll /link /INCREMENTAL:NO || exit /b 1
 
 REM GL Debug DLL
-cl /D_DEBUG /DIMPL /DSOKOL_DLL /DSOKOL_GLCORE c\sokol.c /Z7 /LDd /MDd /DLL /Fe:sokol_dll_windows_x64_gl_debug.dll /link /INCREMENTAL:NO
+cl /D_DEBUG /DIMPL /DSOKOL_DLL /DSOKOL_GLCORE c\sokol.c /Z7 /LDd /MDd /DLL /Fe:sokol_dll_windows_x64_gl_debug.dll /link /INCREMENTAL:NO || exit /b 1
 
 REM GL Release DLL
-cl /D_DEBUG /DIMPL /DSOKOL_DLL /DSOKOL_GLCORE c\sokol.c /LD /MD /DLL /Fe:sokol_dll_windows_x64_gl_release.dll /link /INCREMENTAL:NO
+cl /DNDEBUG /DIMPL /DSOKOL_DLL /DSOKOL_GLCORE c\sokol.c /LD /MD /DLL /Fe:sokol_dll_windows_x64_gl_release.dll /link /INCREMENTAL:NO || exit /b 1
 
-del sokol.obj
+if exist sokol.obj del sokol.obj
+
+exit /b 0
+
+:ensure_msvc
+where cl >nul 2>nul
+if not errorlevel 1 goto :eof
+
+REM DUMBAI: Bootstrap the MSVC environment so this script also works from a plain terminal.
+set "VSWHERE=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+if not exist "%VSWHERE%" (
+    echo ERROR: Could not find vswhere.exe.
+    exit /b 1
+)
+for /f "usebackq delims=" %%I in (`"%VSWHERE%" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do set "VSINSTALL=%%I"
+if not defined VSINSTALL (
+    echo ERROR: Could not find a Visual Studio installation with MSVC tools.
+    exit /b 1
+)
+call "%VSINSTALL%\VC\Auxiliary\Build\vcvarsall.bat" x64 >nul || exit /b 1
+goto :eof
